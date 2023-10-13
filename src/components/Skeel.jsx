ed0d907f399e182c2e9c20a896aaf87772652ad1
Jayesh1512 <131896146+Jayesh1512@users.noreply.github.com>
2023-10-13 23:03:16 +0530

    skills

--- src/components/Skeel.jsx
+++ src/components/Skeel.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function Skeel() {
+function Skeel(props) {
   return (
-    <div>
-      
+    <div className='w-full h-full'>
+        <p className=' text-center text-2xl mb-4 font-bold text-bg-purple'>{props.name.name}</p>
+      <img src={props.name.img} alt="" className="object-contain max-h-40" />
     </div>
   )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import html from '/html.png'
 import Skeel from './Skeel'
 
 
@@ -7,19 +6,32 @@ function Skills() {
 
     const arr = [
         {
-            title: "HTML",
-            img : {html}
-
+            name : "HTML",
+            img : "/html.png"},
+        {
+            name : "CSS",
+            img : "/css.png",
         },
+        {
+            name : "REACT",
+            img : "/atom.png",
+        },
+        // {
+        //     name : "BLENDER",
+        //     img : "/blender.png",
+        // },
+        
     ]
 
 
   return (
-    <div className='w-full h-screen '>
-      {arr.map((p,i) =>{
-        return <Skeel key = {i} obj = {p} />
-      })}
-    </div>
+    
+      <div className="w-full min-h-screen mt-20 gap-8  grid lg:grid-cols-3 md:grid-cols-2 justify-items-stretch max-md:justify-center justify-between items-center md:px-8">
+					{arr.map((p, i) => {
+						return <Skeel key={i} name={p} />;
+					})}
+				</div>
+
   )
 }
 
