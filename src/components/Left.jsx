38da8606cde280fc86c3a818cbf4337f9903cfaf
Jayesh1512 <131896146+Jayesh1512@users.noreply.github.com>
2023-10-20 18:51:13 +0530

    nav bar initiated

--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -26,16 +26,23 @@ function Left() {
 
   return (
     <div className='w-full'>
-      <div className=' ml-8 flex justify-start w-9/12 lg:ml-14'>
-      <button className='mt-20 w-10 h-10 rounded-full bg-text-pink grid place-content-center' onClick={pinkTheme}></button>
-      <button className='ml-20 mt-20 w-10 h-10 rounded-full bg-text-yellow grid place-content-center' onClick={yellowTheme}></button>
-      <button className='ml-20 mt-20 w-10 h-10 rounded-full bg-text-green grid place-content-center' onClick={greenTheme}></button>
-          
-      </div>
+      <div className='flex justify-between items-start lg:flex-row-reverse w-9/12 lg:ml-14 '>
+        <div className='flex ml-8'>
+            <button className='mt-20 w-10 h-10 rounded-full bg-text-pink grid place-content-center' onClick={pinkTheme}></button>
+            <button className='ml-20 mt-20 w-10 h-10 rounded-full bg-text-yellow grid place-content-center' onClick={yellowTheme}></button>
+            <button className='ml-20 mt-20 w-10 h-10 rounded-full bg-text-green grid place-content-center' onClick={greenTheme}></button>   
+        </div>
+
+        <div>
+            <button className='mt-20 w-20 h-20 rounded-full reverse grid place-content-center p-3 lg:relative fixed right-10 bottom-20 z-50 '>
+              <img src={image} alt="" className='object-contain'/>
+            </button>
+        </div>
         
+      </div>
  
         {/* Text */}
-          <span className='h-2/5 mt-24  flex  flex-col items-center w-5/6   gap-10 '>
+          <span className='h-2/5 mt-20  flex  flex-col items-center w-5/6   gap-10 '>
             <p className='font-main content text-5xl font-medium w-5/6'  >Hello. I'm a User Interface Develooper </p>
             <p className='font-main content text-lg w-5/6 font-normal' >My name is Jayesh Shete . I craft user interface using modern front end technology</p>
           </span>
